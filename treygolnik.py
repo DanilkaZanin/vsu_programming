a30f7d06057a3a28f2dfff5a4388a21e49c878fc
DanilkaZanin <49477090+DanilkaZanin@users.noreply.github.com>
2020-04-20 17:23:36 +0300

    Delete treygolnik.py

--- treygolnik.py
+++
@@ -1,9 +0,0 @@
-print("Vvedite dliny storon")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
- 
-if a + b > c and a + c > b and b + c > a:
-	print("True")
-else:
-	print("False")
