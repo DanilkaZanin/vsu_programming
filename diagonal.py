e83ce9689eca69dfc12a2570d7a66a103737cd62
DanilkaZanin <49477090+DanilkaZanin@users.noreply.github.com>
2020-04-27 14:04:47 +0300

    Update diagonal.py

--- diagonal.py
+++ diagonal.py
@@ -1,21 +1,27 @@
-def helper(b):
-    c = [1, *b]
-    for i in range(1, len(c) - 1):
-        c[i] += c[i + 1]
-    return c
+
+import time
+
+def f():
+	time.sleep(2)
+
 
 def diagonal(level, num):
 	if not num:
 		return level
-	b = []
-	values = []
-	for i in range(level+1):
-		b = helper(b)
-		values.append(b)
-	return sum([values[i][-(num+1)] for i in range(num, len(values))])
+	values = [[1], [1, 1]]
+	for i in range(2, level + 1):
+		row = []
+		for j in range(len(values[i-1]) - 1):
+			row.append(values[i-1][j] + values[i-1][j+1])
+		values.append([1, *row, 1])
+	return sum([i[num] for i in values [num:]])
 
 print(diagonal(7, 0))
 print(diagonal(7, 1))
 print(diagonal(7, 2))
 print(diagonal(20, 3))
 print(diagonal(20, 4))
+
+t1 = time.time()
+f() 
+print(time.time() - t1)
