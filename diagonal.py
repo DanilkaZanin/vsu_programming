aa987145ca84090c6c18e2844406fc2a6463ea72
DanilkaZanin <49477090+DanilkaZanin@users.noreply.github.com>
2020-05-24 00:01:21 +0300

    ez

--- diagonal.py
+++ diagonal.py
@@ -1,27 +1,16 @@
+def c(n, k):
+	if 0 <= k <= n:
+		n = n + 1
+		k = k + 1
+		nn = 1
+		kk = 1
+		for t in range(1, min(k, n - k) + 1):
+			nn *= n
+			kk *= t
+			n -=1
+		return nn//kk
+	else:
+		return 0
 
-import time
-
-def f():
-	time.sleep(2)
-
-
-def diagonal(level, num):
-	if not num:
-		return level
-	values = [[1], [1, 1]]
-	for i in range(2, level + 1):
-		row = []
-		for j in range(len(values[i-1]) - 1):
-			row.append(values[i-1][j] + values[i-1][j+1])
-		values.append([1, *row, 1])
-	return sum([i[num] for i in values [num:]])
-
-print(diagonal(7, 0))
-print(diagonal(7, 1))
-print(diagonal(7, 2))
-print(diagonal(20, 3))
-print(diagonal(20, 4))
-
-t1 = time.time()
-f() 
-print(time.time() - t1)
+print(c(7, 2))
+print(c(2**100, 16))
