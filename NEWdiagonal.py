00af84d00e38453676c8c0145bb4ed9f6a2a6c1d
DanilkaZanin <49477090+DanilkaZanin@users.noreply.github.com>
2020-05-24 12:05:15 +0300

    Update NEWdiagonal.py

--- NEWdiagonal.py
+++ NEWdiagonal.py
@@ -1,9 +1,7 @@
 import math 
 
 def func(line, diagonal):
-	line = line + 1
-	diagonal = diagonal + 1
-	return math.comb(line, diagonal)
+	return math.comb(line + 1, diagonal + 1)
 
 print(func(7, 2))
 print(func(2**100, 16))
